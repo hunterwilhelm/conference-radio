594913ed92d1cbcfb6d6245a236420a71bc0647e
Hunter Wilhelm <71348224+hunterwilhelm@users.noreply.github.com>
2023-09-24 17:34:19 -0600

    update support and privacy policy

--- website-conferenceradio-app/src/pages/_app.tsx
+++ website-conferenceradio-app/src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type NextPage } from "next";
+import { type AppProps } from "next/app";
+import { type ReactElement, type ReactNode } from "react";
+import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
 };
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+const queryClient = new QueryClient();
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withCssVariables>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default MyApp;
